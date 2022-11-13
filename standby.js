721e91907c4174f27c6bafad112fa5fd48aaa576
test <test@test.com>
2022-11-14 03:25:41 +0900

    feat: all update

--- standby.js
+++ standby.js
@@ -1,4 +1,4 @@
-const bgcolor = "rgb(16, 6, 159)";
+const bgcolor = "rgb(0, 28, 152)";
 
 function add_img() {
   let img = document.createElement("img");
@@ -19,80 +19,159 @@ function add_img() {
   document.querySelector(".gifContainer").appendChild(img2);
 }
 
-window.addEventListener(
-  "resize",
-  function (event) {
-    let windowWidth = window.matchMedia("screen and (max-width: 580px)");
-    if (windowWidth.matches) {
-      console.log("모바일이여라");
-      console.log(document.querySelector(".gifContainer"));
+function add_img_desk() {
+  let img = document.createElement("img");
+  img.src = "./assets/standby/desk_blue.png";
+  img.className = "deskBlue";
+  img.style.width = "100vw";
+  img.style.display = "block";
+  document.querySelector(".deskContainer").appendChild(img);
 
-      const gifContainer = document.querySelector(".gifContainer");
-      gifContainer.style.display = "block";
-      if (document.querySelector(".gif_white") == null) {
-        add_img();
-      }
-      const whiteSide = document.querySelector(".gif_white");
-      const blueSide = document.querySelector(".gif_blue");
+  let img2 = document.createElement("img");
+  img2.src = "./assets/standby/desk_white.png";
+  img2.className = "deskWhite";
+  img2.style.width = "100vw";
+  img2.style.display = "none";
+  document.querySelector(".deskContainer").appendChild(img2);
+}
 
-      function handleGif() {
-        if (blueSide.style.display === "none") {
-          whiteSide.style.display = "none";
-          gifContainer.style.background = "white";
-          blueSide.style.display = "block";
-        } else {
-          whiteSide.style.display = "block";
-          gifContainer.style.background = bgcolor;
-          blueSide.style.display = "none";
-        }
-      }
+window.onload = function () {
+  let windowWidth = window.matchMedia("screen and (max-width: 580px)");
+  if (windowWidth.matches) {
+    console.log("모바일이여라");
+    console.log(document.querySelector(".gifContainer"));
 
-      makeGif = setInterval(function () {
-        console.log("start!");
-        handleGif();
-      }, 400);
+    const deskContainer = document.querySelector(".deskContainer");
+    deskContainer.style.display = "none";
 
-      gifContainer.addEventListener("click", () => {
-        clearInterval(makeGif);
-        gifContainer.rel = "noopener noreferrer";
-        gifContainer.href = "https://naver.me/FXHu9h6y";
-      });
-    } else {
-      clearInterval(makeGif);
-      console.log("PC 여라");
-      const gifContainer = document.querySelector(".gifContainer");
-      gifContainer.style.display = "none";
-
-      let img = document.createElement("img");
-      img.src = "./assets/standby/desk_blue.png";
-      img.class = "deskBlue";
-      img.style = "display: block";
-      document.querySelector(".gifContainer").appendChild(img);
-
-      let img2 = document.createElement("img");
-      img2.src = "./assets/standby/desk_white.png";
-      img2.class = "deskWhite";
-      img2.style = "display: none";
-      document.querySelector(".gifContainer").appendChild(img2);
-
-      const deskContainer = document.querySelector(".deskContainer");
-      deskContainer.style.background = bgcolor;
-
-      const deskBlue = document.querySelector(".deskBlue");
-      const deskWhite = document.querySelector(".deskWhite");
-
-      deskContainer.addEventListener("click", () => {
-        if ((deskContainer.style.background = bgcolor)) {
-          deskBlue.style.display = "block";
-          deskContainer.style.background = bgcolor;
-          deskWhite.style.display = "none";
-        } else {
-          deskBlue.style.display = "none";
-          deskContainer.style.background = bgcolor;
-          deskWhite.style.display = "block";
-        }
-      });
+    const gifContainer = document.querySelector(".gifContainer");
+    gifContainer.style.display = "flex";
+    if (document.querySelector(".gif_white") == null) {
+      add_img();
     }
-  },
-  true
-);
+    const whiteSide = document.querySelector(".gif_white");
+    const blueSide = document.querySelector(".gif_blue");
+
+    function handleGif() {
+      if (blueSide.style.display === "none") {
+        whiteSide.style.display = "none";
+        gifContainer.style.background = bgcolor;
+        blueSide.style.display = "block";
+      } else {
+        whiteSide.style.display = "block";
+        gifContainer.style.background = "white";
+        blueSide.style.display = "none";
+      }
+    }
+
+    makeGif = setInterval(function () {
+      console.log("start!");
+      handleGif();
+    }, 400);
+
+    gifContainer.addEventListener("click", () => {
+      clearInterval(makeGif);
+      gifContainer.rel = "noopener noreferrer";
+      gifContainer.href = "https://naver.me/FXHu9h6y";
+    });
+  } else {
+    console.log("PC 여라");
+    const gifContainer = document.querySelector(".gifContainer");
+    gifContainer.style.display = "none";
+
+    if (document.querySelector(".deskBlue") == null) {
+      add_img_desk();
+    }
+
+    const deskContainer = document.querySelector(".deskContainer");
+
+    const deskBlue = document.querySelector(".deskBlue");
+    const deskWhite = document.querySelector(".deskWhite");
+
+    deskContainer.addEventListener("click", () => {
+      // console.log("clasdfasuf");
+      // console.log(deskBlue.style.display);
+      if (deskBlue.style.display == "none") {
+        deskBlue.style.display = "block";
+        deskContainer.style.background = bgcolor;
+        deskWhite.style.display = "none";
+      } else {
+        deskBlue.style.display = "none";
+        deskContainer.style.background = "white";
+        deskWhite.style.display = "block";
+      }
+    });
+  }
+};
+
+window.addEventListener("resize", () => {
+  window.location.reload();
+});
+
+// window.addEventListener(
+//   "resize",
+//   function (event) {
+//     let windowWidth = window.matchMedia("screen and (max-width: 580px)");
+//     if (windowWidth.matches) {
+//       console.log("모바일이여라");
+//       console.log(document.querySelector(".gifContainer"));
+
+//       const gifContainer = document.querySelector(".gifContainer");
+//       gifContainer.style.display = "block";
+//       if (document.querySelector(".gif_white") == null) {
+//         add_img();
+//       }
+//       const whiteSide = document.querySelector(".gif_white");
+//       const blueSide = document.querySelector(".gif_blue");
+
+//       function handleGif() {
+//         if (blueSide.style.display === "none") {
+//           whiteSide.style.display = "none";
+//           gifContainer.style.background = "white";
+//           blueSide.style.display = "block";
+//         } else {
+//           whiteSide.style.display = "block";
+//           gifContainer.style.background = bgcolor;
+//           blueSide.style.display = "none";
+//         }
+//       }
+
+//       makeGif = setInterval(function () {
+//         console.log("start!");
+//         handleGif();
+//       }, 400);
+
+//       gifContainer.addEventListener("click", () => {
+//         clearInterval(makeGif);
+//         gifContainer.rel = "noopener noreferrer";
+//         gifContainer.href = "https://naver.me/FXHu9h6y";
+//       });
+//     } else {
+//       console.log("PC 여라");
+//       const gifContainer = document.querySelector(".gifContainer");
+//       gifContainer.style.display = "none";
+
+//       if (document.querySelector(".deskBlue") == null) {
+//         add_img_desk();
+//       }
+//       const deskContainer = document.querySelector(".deskContainer");
+
+//       const deskBlue = document.querySelector(".deskBlue");
+//       const deskWhite = document.querySelector(".deskWhite");
+
+//       deskContainer.addEventListener("click", () => {
+//         console.log(deskBlue.style.display);
+//         if (deskBlue.style.display != "none") {
+//           deskBlue.style.display = "block";
+//           deskContainer.style.background = bgcolor;
+//           deskWhite.style.display = "none";
+//         } else {
+//           deskBlue.style.display = "none";
+//           deskContainer.style.background = bgcolor;
+//           deskWhite.style.display = "block";
+//         }
+//       });
+//     }
+//   },
+//   true
+// );
